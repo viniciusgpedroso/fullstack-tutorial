0cc38aefacf9086cd1bad04b7c289663cf2b6d4f
Vinicius Pedroso <vpedroso@profusion.mobi>
2020-12-03 11:36:56 -0300

    Add Step: 2 Connect to data sources

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,15 +1,25 @@
-require('dotenv').config();
+require("dotenv").config();
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const { createStore } = require("./utils");
 
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
+const LaunchAPI = require("./datasources/launch");
+const UserAPI = require("./datasources/user");
 
-const server = new ApolloServer({ typeDefs });
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store }),
+  }),
+});
 
 server.listen().then(() => {
-    console.log(`
+  console.log(`
       Server is running!
       Listening on port 4000
       Explore at https://studio.apollographql.com/dev
     `);
-  });
-  
+});
